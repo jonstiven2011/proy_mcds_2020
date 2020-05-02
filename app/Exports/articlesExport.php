35799723f1689d60070f91788c00e9e453622663
jonstiven2011 <jonstiven2011@hotmail.com>
2020-05-02 12:12:21 -0500

    editor

--- app/Exports/articlesExport.php
+++ app/Exports/articlesExport.php
@@ -15,8 +15,15 @@ class ArticlesExport implements FromView
 {
     public function view(): View
     {
+        
+        if(Auth::user()->role == 'admin'){
+            $articles = Article::paginate(8);
+        }else if(Auth::user()->role == 'editor'){
+            $articles = Article::where('user_id', '=', Auth::user()->id)->paginate(8);
+        }
         return view('articles.excel',[
-            'users' => User::all(),
+            //'users' => User::all(),
+            'articles' => $articles,
 			'categories' => Category::all(),
             'articles' => Article::all()
         ]);
